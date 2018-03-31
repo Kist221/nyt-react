ce6582d566b9a302d8fe668d18c38c82e79e98d6
Kist221 <Kist221@gmail.com>
2018-03-30 20:25:56 -0700

    closer?

--- client/src/pages/Articles/Articles.js
+++ client/src/pages/Articles/Articles.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import Jumbotron from "../../components/Jumbotron";
 import API from "../../utils/API";
 import DeleteBtn from "../../components/DeleteBtn";
+import { Input, FormBtn } from "../../components/Form";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn } from "../../components/Form";
 
 class Articles extends Component {
   state = {
-    articles: []
+    articles: [],
+    search: ""
   };
 
   componentDidMount() {
@@ -21,24 +22,35 @@ class Articles extends Component {
       .catch(err => console.log(err));
   };
 
+  // Handles updating component state when the user types into the input field
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+  
   render() {
     return (
       <Container fluid>
         <Row>
-          <Col size="md-6">
+          <Col size="sm-12">
             <Jumbotron>
-              <h1>What Articles Should I Read?</h1>
-            </Jumbotron>
-            <form>
-              <Input name="title" placeholder="Title (required)" />
-              <Input name="author" placeholder="Author (required)" />
-              <TextArea name="synopsis" placeholder="Synopsis (Optional)" />
-              <FormBtn>Submit Article</FormBtn>
-            </form>
-          </Col>
-          <Col size="md-6 sm-12">
-            <Jumbotron>
-              <h1>Articles On My List</h1>
+              <h1>NYT Articles</h1>
+              <form>
+                <Input 
+                value={this.state.search}
+                onChange={this.handleInputChange}
+                name="search" 
+                placeholder="Search" 
+                />
+                <FormBtn
+                disabled={!this.state.search}
+                onClick={this.handleFormSubmit}
+                >
+                  Search
+                </FormBtn>
+              </form>
             </Jumbotron>
             {this.state.articles.length ? (
               <List>
